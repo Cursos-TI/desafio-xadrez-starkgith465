03424a88c8073a04147aa8ded5dd9befcda4d72e
starkgith465 <matheus.santos.gith@gmail.com>
2025-03-14 00:41:46 +0000

    bispo

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-        
+       // nada
 
 
 
